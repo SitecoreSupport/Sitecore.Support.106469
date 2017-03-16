aca2c23d9d5a2dc29fd154519d41cc4c2a877022
ViacheslavAfanasyev <sadney7@gmail.com>
2017-03-16 14:21:23 +0200

    First commit

---
+++ src/Sitecore.Support.106469/CreateContactInXDb.cs
@@ -0,0 +1,45 @@
+﻿using Sitecore.Analytics.Model.Entities;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Commerce.Data.Customers;
+using Sitecore.Commerce.Entities;
+using Sitecore.Commerce.Services.Customers;
+using CreateContactInXDbProcessor = Sitecore.Commerce.Pipelines.Customers.CreateContact.CreateContactInXDb;
+
+
+namespace Sitecore.Support.Commerce.Pipelines.Customers.CreateContact
+{
+    /// <summary>
+    /// Creates Contact in xDb
+    /// </summary>
+    /// <seealso cref="Sitecore.Commerce.Pipelines.Customers.CreateContact.CreateContactInXDb" />
+    public class CreateContactInXDb : CreateContactInXDbProcessor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateContactInXDb"/> class.
+        /// </summary>
+        /// <param name="userRepository">The user repository.</param>
+        /// <param name="entityFactory">The entity factory.</param>
+        public CreateContactInXDb(IUserRepository userRepository, IEntityFactory entityFactory)
+            : base(userRepository, entityFactory)
+        {
+        }
+
+        /// <summary>
+        /// Adds the facets.
+        /// </summary>
+        /// <param name="contact">The contact.</param>
+        /// <param name="createUserResult">The create user result.</param>
+        protected override void AddFacets(Contact contact, CreateUserResult createUserResult)
+        {
+            var emailAddress = contact.GetFacet<IContactEmailAddresses>("Emails");
+
+            if (emailAddress.Entries.Contains("main"))
+            {
+                emailAddress.Preferred = "main";
+                emailAddress.Entries["main"].SmtpAddress = createUserResult.CommerceUser.Email;
+                return;
+            }
+            base.AddFacets(contact, createUserResult);
+        }
+    }
+}
